59d0768bf914d9fabb6be49900387f25541d9ea2
Ali0316 <mr.alinasir40@gmail.com>
2024-02-04 03:20:02 +0500

    Headers

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // app.options("*", cors())
 
-app.use(cors({
-    origin: process.env.ORIGIN,
-    credentials:true,
-}))
+// app.use(cors({
+//     origin: process.env.ORIGIN,
+//     credentials:true,
+// }))
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,DELETE,PATCH,OPTIONS"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
@@ -17,7 +29,7 @@ app.use(cookieParser());
 const cards = require("./routes/cardRoute");
 const user = require("./routes/userRoute");
 
-app.use("/api/v2",cards);
-app.use("/api/v1",user);
+app.use("/api/v2", cards);
+app.use("/api/v1", user);
 
 module.exports = app;
